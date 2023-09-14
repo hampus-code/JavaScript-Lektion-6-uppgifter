ffaa757d275b0f804c3031948a2883a9b26da117
hampus-code <hampusss96@gmail.com>
2023-09-14 12:14:38 +0200

    Added Variables and some style

---
+++ logicjs.js
@@ -0,0 +1,15 @@
+function changeHeaderStyle() {
+    document.getElementById("header-style").style.color = "blue" //Ändrar färg på h1
+    document.getElementById("header-style").style.border = "thick solid red" // Ger en border runt h1
+}
+
+function varItems() {
+
+    var wholenumber = 2;
+
+    var decimalnumber = 0.7;
+
+    var text = "A Text Variable"
+
+    var boolean = true
+}
